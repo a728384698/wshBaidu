f7667aab248375990db4d3ee6b27642a5e98864c
xyj321 <xyj321@hotmail.com>
2022-06-23 15:48:40 +0800

    测试 是否有弹框

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+/*
+ * @Date: 2022-06-23 09:22:43
+ * @LastEditors: 魏盛华
+ * @LastEditTime: 2022-06-23 15:48:14
+ * @Author: 魏盛华
+ */
+import { NativeModules } from 'react-native'
+import _BaiduViewManage from './js/BaiduViewManage'
 
-import { NativeModules } from 'react-native';
+// const { RNXyjBaidu } = NativeModules
+export const BaiduViewManage = _BaiduViewManage
 
-const { RNXyjBaidu } = NativeModules;
-
-export default RNXyjBaidu;
+export default RNXyjBaidu
--- js/BaiduViewManage.js
+++ js/BaiduViewManage.js
@@ -1,15 +1,13 @@
 /*
  * @Date: 2022-06-23 11:05:32
  * @LastEditors: 魏盛华
- * @LastEditTime: 2022-06-23 11:23:54
+ * @LastEditTime: 2022-06-23 15:43:32
  * @Author: 魏盛华
  */
 import { Alert } from 'react-native'
 
-class BaiduViewManage {
+export default {
   initAlert(message) {
     Alert.alert('温馨提醒', message)
-  }
+  },
 }
-
-export default BaiduViewManage
